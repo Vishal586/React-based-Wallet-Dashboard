fcdbb6b0ad1c7d25de1141a9257b8a284a9b0117
Vishal586 <128839643+Vishal586@users.noreply.github.com>
2025-06-25 22:23:46 +0530

    Add files via upload

---
+++ src/data/navigationLinks.js
@@ -0,0 +1,12 @@
+export const navigationLinks = [
+  { id: 1, title: 'Home', sectionId: 'home', image: '/icons/home.png' },
+  { id: 2, title: 'Budget', sectionId: 'budget', image: '/icons/budget.png' },
+  { id: 3, title: 'Transactions', sectionId: 'transactions', image: '/icons/transactions.png' },
+  { id: 4, title: 'Subscriptions', sectionId: 'subscriptions', image: '/icons/subscriptions.png' },
+  { id: 5, title: 'Loans', sectionId: 'loans', image: '/icons/loans.png' },
+  { id: 6, title: 'Reports', sectionId: 'reports', image: '/icons/reports.png' },
+  { id: 7, title: 'Savings', sectionId: 'savings', image: '/icons/savings.png' },
+  { id: 8, title: 'Financial Advice', sectionId: 'advice', image: '/icons/advice.png' },
+  { id: 9, title: 'Account', sectionId: 'account', image: '/icons/account.png' },
+  { id: 10, title: 'Settings', sectionId: 'settings', image: '/icons/settings.png' },
+];
